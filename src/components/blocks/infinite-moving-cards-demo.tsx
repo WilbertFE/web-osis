35c017bb6e3243ea0302b54000c079f5552c9a56
WilbertFE <wilbertbernadi699@gmail.com>
2025-07-03 17:29:43 +0700

    feat: add  infinite moving card content

--- src/components/blocks/infinite-moving-cards-demo.tsx
+++ src/components/blocks/infinite-moving-cards-demo.tsx
@@ -18,31 +18,26 @@ export function InfiniteMovingCardsDemo() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+      "menurut saya OSIS adalah wadah dimana saya bisa belajar mengembangkan keterampilan dan jiwa kepemimpinan, di OSIS saya banyak belajar tentang komunikasi, berpendapat, dan mengambil keputusan",
+    name: "XI-1",
+    title: "Helend",
   },
   {
     quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
-  },
-  {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
+      "OSIS adalah organisasi resmi bagi para siswa di sekolah untuk belajar bekerjasama,berorganisasi, dan mengembangkan diri.Anggotanya adalah siswa-siswi yang dipilih untuk mewakili teman-temannya, dan mereka bekerja sama menyelenggarakan berbagai kegiatan sekolah, seperti lomba, acara seni, olahraga, upacara, bahkan kegiatan sosial lainnya.",
+    name: "XI-1",
+    title: "Mazmur",
   },
   {
     quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+      "Adalah tempat dimana siswa mampu mengasah dan mengembangkan soft skill dan mengembangkan kemampuan kerjasama yang terstruktur dalam kelompok dengan masih memikul beban tanggung jawab.",
+    name: "XI-3",
+    title: "Jacob",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+      "tanggapan saya, OSIS adalah tempat bagi para siswa untuk merepresentasikan suara mereka serta mendorong peningkatan perkembangan karakter dan kepimpinan mereka.",
+    name: "X-3",
+    title: "Queen",
   },
 ];
