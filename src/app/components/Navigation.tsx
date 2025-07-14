c7edfe64912d20c3c889df05a917c16109f2948f
WilbertFE <wilbertbernadi699@gmail.com>
2025-07-14 17:15:43 +0700

    fix: login google button size

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -35,9 +35,9 @@ export function Navigation() {
                   callbackUrl: "/",
                 })
               }
-              className="px-4 mx-2 cursor-pointer py-0.5 flex items-center gap-x-2 font-bold border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+              className="px-2 mx-2 cursor-pointer py-0.5 flex items-center gap-x-1 border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
             >
-              <FcGoogle size={32} />
+              <FcGoogle size={24} />
               Login
             </button>
           )}
